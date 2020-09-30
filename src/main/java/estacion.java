a1b0e4a5f76ef27bb42e9fec44aa8ed1d7c318a1
Jonathan Prieto <jonathan.prieto@me.com>
2020-09-30 10:09:03 -0500

    Cambios Leves

--- src/main/java/estacion.java
+++ src/main/java/estacion.java
@@ -1,7 +1,7 @@
 import java.util.Observable;
 
 public class estacion extends Observable {
-    private double[] datos;
+    private final double[] datos = new double[3];
 
     public void addData(double temp, double humidity, double windspeed) {
         setChanged();
