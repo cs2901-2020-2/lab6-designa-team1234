cd1ac5d4b4f376b6f9bbdcfa26112dd3a5b188f1
v1c7or20 <victor.pena@utec.edu.pe>
2020-09-30 10:01:24 -0500

    prototipos de funciones test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,39 @@
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class test {
+    @Test
+    public void test_get_data_single() {
+        estacion estacion_principal = new estacion();
+        cliente nuevoCliente = new cliente();
+        nuevoCliente.getData(es);
+    }
+
+    @Test
+    public void test_get_data_multiple() {
+
+    }
+
+    @Test
+    public void test_data_changes_single() {
+
+    }
+
+    @Test
+    public void test_data_changes_multiple() {
+
+    }
+
+    @Test
+    public void tetst_data_create_single() {
+
+    }
+
+    @Test
+    public void test_data_create_multiple() {
+
+    }
 }
