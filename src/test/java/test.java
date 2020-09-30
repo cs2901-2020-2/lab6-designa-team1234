851c9df0d4f568e2df49bb1634bb711ad22effe2
v1c7or20 <victor.pena@utec.edu.pe>
2020-09-30 18:16:59 -0500

    cambios menores

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -10,22 +10,22 @@ import java.util.Scanner;
 public class test {
     @Test
     public void test_get_data_single() {
-        estacion estacion_principal = new estacion();
-        cliente nuevoCliente = new cliente(0);
-        estacion_principal.addData(32.0,43.33,56.0);
+        EstacionMeteorologica estacion_principal = new EstacionMeteorologica();
+        Cliente nuevoCliente = new Cliente(0);
         estacion_principal.addObserver(nuevoCliente);
+        estacion_principal.addData(32.0,43.33,56.0);
 
         Assert.assertEquals(nuevoCliente.getData(), estacion_principal.getData());
     }
 
     @Test
     public void test_get_data_multiple() {
-        estacion estacion_principal = new estacion();
-        cliente nuevoCliente0 = new cliente(0);
-        cliente nuevoCliente1 = new cliente(1);
-        cliente nuevoCliente2 = new cliente(2);
-        cliente nuevoCliente3 = new cliente(3);
-        cliente nuevoCliente4 = new cliente(4);
+        EstacionMeteorologica estacion_principal = new EstacionMeteorologica();
+        Cliente nuevoCliente0 = new Cliente(0);
+        Cliente nuevoCliente1 = new Cliente(1);
+        Cliente nuevoCliente2 = new Cliente(2);
+        Cliente nuevoCliente3 = new Cliente(3);
+        Cliente nuevoCliente4 = new Cliente(4);
 
         estacion_principal.addObserver(nuevoCliente0);
         estacion_principal.addObserver(nuevoCliente1);
@@ -44,8 +44,8 @@ public class test {
 
     @Test
     public void test_data_changes_single() {
-        estacion estacion_principal = new estacion();
-        cliente nuevoCliente0 = new cliente(1);
+        EstacionMeteorologica estacion_principal = new EstacionMeteorologica();
+        Cliente nuevoCliente0 = new Cliente(1);
         estacion_principal.addObserver(nuevoCliente0);
 
         estacion_principal.addData(32.0,43.33,56.0);
@@ -58,12 +58,12 @@ public class test {
 
     @Test
     public void test_data_changes_multiple() {
-        estacion estacion_principal = new estacion();
-        cliente nuevoCliente0 = new cliente(0);
-        cliente nuevoCliente1 = new cliente(1);
-        cliente nuevoCliente2 = new cliente(2);
-        cliente nuevoCliente3 = new cliente(3);
-        cliente nuevoCliente4 = new cliente(4);
+        EstacionMeteorologica estacion_principal = new EstacionMeteorologica();
+        Cliente nuevoCliente0 = new Cliente(0);
+        Cliente nuevoCliente1 = new Cliente(1);
+        Cliente nuevoCliente2 = new Cliente(2);
+        Cliente nuevoCliente3 = new Cliente(3);
+        Cliente nuevoCliente4 = new Cliente(4);
 
         estacion_principal.addObserver(nuevoCliente0);
         estacion_principal.addObserver(nuevoCliente1);
@@ -99,10 +99,10 @@ public class test {
 
     @Test
     public void tetst_data_create_single() {
-        estacion estacion_principal = new estacion();
-        cliente nuevoCliente0 = new cliente(0);
+        EstacionMeteorologica estacion_principal = new EstacionMeteorologica();
+        Cliente nuevoCliente0 = new Cliente(0);
         estacion_principal.addObserver(nuevoCliente0);
-        cliente nuevoCliente1 = new cliente(1);
+        Cliente nuevoCliente1 = new Cliente(1);
 
         estacion_principal.addData(32.0,43.33,56.0);
         Assert.assertEquals(nuevoCliente0.getData(), estacion_principal.getData());
@@ -120,21 +120,21 @@ public class test {
 
     @Test
     public void test_data_create_multiple() {
-        estacion estacion_principal = new estacion();
-        cliente nuevoCliente0 = new cliente(0);
+        EstacionMeteorologica estacion_principal = new EstacionMeteorologica();
+        Cliente nuevoCliente0 = new Cliente(0);
         estacion_principal.addObserver(nuevoCliente0);
 
         estacion_principal.addData(32.0,43.33,56.0);
         Assert.assertEquals(nuevoCliente0.getData(), estacion_principal.getData());
 
-        cliente nuevoCliente1 = new cliente(1);
+        Cliente nuevoCliente1 = new Cliente(1);
         estacion_principal.addObserver(nuevoCliente1);
 
         estacion_principal.addData(27.0,80.34,23.4);
         Assert.assertEquals(nuevoCliente0.getData(), estacion_principal.getData());
         Assert.assertEquals(nuevoCliente1.getData(), estacion_principal.getData());
 
-        cliente nuevoCliente2 = new cliente(2);
+        Cliente nuevoCliente2 = new Cliente(2);
         estacion_principal.addObserver(nuevoCliente2);
 
         estacion_principal.addData(23.2,55.33,16.0);
@@ -146,16 +146,16 @@ public class test {
 
     @Test(expectedExceptions = Exception.class)
     public void UpdateClientTest() throws Exception{
-        estacion estacionMetereologica = new estacion();
-        cliente client1 = new cliente(1);
-        cliente client2 = new cliente(2);
+        EstacionMeteorologica estacionMetereologica = new EstacionMeteorologica();
+        Cliente client1 = new Cliente(1);
+        Cliente client2 = new Cliente(2);
         estacionMetereologica.addData(23.4,12.2,43.2);
         double[] getValue = client1.getData();
         Assert.assertEquals(getValue[0], 23.4);
         Assert.assertEquals(getValue[1], 12.2);
         Assert.assertEquals(getValue[2], 43.2);
         estacionMetereologica.addData(54.5,12.2,56.1);
-        cliente client3 = new cliente(3);
+        Cliente client3 = new Cliente(3);
         double[] getValue2 = client3.getData();
         Assert.assertEquals(getValue2[0], 54.5);
         Assert.assertEquals(getValue2[1], 12.2);
