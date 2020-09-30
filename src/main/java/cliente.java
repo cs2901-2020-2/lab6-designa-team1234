8f989111d71260cc33459e6cb1030a11a0f434b3
v1c7or20 <victor.pena@utec.edu.pe>
2020-09-30 10:41:41 -0500

    mitad de implementacion

---
+++ src/main/java/cliente.java
@@ -0,0 +1,18 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class cliente implements Observer{
+    private int id;
+    private double []datos = new double[3];
+    public cliente(int id) {
+        this.id = id;
+    }
+
+    public void update(Observable estacion, Object values) {
+        this.datos = (double []) values;
+    }
+
+    public double[] getData() {
+        return datos;
+    }
+} 
