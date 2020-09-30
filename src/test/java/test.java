d86bf4374fc8a9ad90eee5f6b6919e11db9d58b8
v1c7or20 <victor.pena@utec.edu.pe>
2020-09-30 18:41:33 -0500

    succesfull tests

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,6 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.io.InputStream;
-import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -144,46 +143,28 @@ public class test {
 
     }
 
-    @Test(expectedExceptions = Exception.class)
-    public void UpdateClientTest() throws Exception{
+    @Test
+    public void UpdateClientTest() {
         EstacionMeteorologica estacionMetereologica = new EstacionMeteorologica();
         Cliente client1 = new Cliente(1);
         Cliente client2 = new Cliente(2);
+        estacionMetereologica.addObserver(client1);
+        estacionMetereologica.addObserver(client2);
+
         estacionMetereologica.addData(23.4,12.2,43.2);
         double[] getValue = client1.getData();
         Assert.assertEquals(getValue[0], 23.4);
         Assert.assertEquals(getValue[1], 12.2);
         Assert.assertEquals(getValue[2], 43.2);
-        estacionMetereologica.addData(54.5,12.2,56.1);
+
         Cliente client3 = new Cliente(3);
+        estacionMetereologica.addObserver(client3);
+        estacionMetereologica.addData(54.5,12.2,56.1);
         double[] getValue2 = client3.getData();
         Assert.assertEquals(getValue2[0], 54.5);
         Assert.assertEquals(getValue2[1], 12.2);
         Assert.assertEquals(getValue2[2], 56.1);
     }
 
-    private List<String> readInput(int testNumber){
-        List<String> lines = readFile(testNumber, "input");
-        return lines;
-    }
-
-    private List<String> readOutput(int testNumber){
-        List<String> lines = readFile(testNumber, "output");
-        return lines;
-    }
-
-    public List<String> readFile(int testNumber, String type){
-        String fileName = "test_case<testNumber>_<type>";
-        fileName = fileName.replace("<testNumber>", Integer.toString(testNumber));
-        fileName = fileName.replace("<type>", type);
-        InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
-        Scanner scan = new Scanner(is);
-        List<String> lines = new ArrayList<String>();
-        while(scan.hasNextLine()) {
-            String line = scan.nextLine();
-            lines.add(line);
-        }
-        return lines;
-    }
 }
 
